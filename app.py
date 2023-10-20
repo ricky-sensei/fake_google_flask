cbef34c54bc354c841110481ac05d859fafcd553
ricky-sensei <axcelcamp.teacher@gmail.com>
2023-10-20 11:19:18 +0900

    フェイクサイトを用意。flaskも用意

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        # Handle POST Request here
+        print("abcefg")
+        return render_template("fake_google.html")
+    
+    return render_template("fake_google.html")
+
+
+if __name__ == "__main__":
+    # DEBUG is SET to TRUE. CHANGE FOR PROD
+    app.run(port=5000, debug=True)
